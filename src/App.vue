f321ff30958185e4449c637593bd8bf627c5fafe
Andrey Mustaev <mustaev-08@yandex.ru>
2019-09-08 22:41:03 +0300

    Added Contracts-Table

--- src/App.vue
+++ src/App.vue
@@ -146,20 +146,12 @@
       <div class="ui-content-body">
 
         <div class="ui-container">
-
+          <h2>{{this.$route.name}}</h2>
           <div class="row">
             <div class="col-md-12">
               <div class="panel">
-                <header class="panel-heading">
-                  Panel heading
-                  <span class="tools pull-right">
-                                            <a class="refresh-box fa fa-repeat" href="javascript:;"></a>
-                                            <a class="collapse-box fa fa-chevron-down" href="javascript:;"></a>
-                                            <a class="close-box fa fa-times" href="javascript:;"></a>
-                                        </span>
-                </header>
                 <div class="panel-body">
-                  Panel body
+                  <router-view></router-view>
                 </div>
               </div>
             </div>
---
+++ src/views/Contracts.vue
@@ -0,0 +1,77 @@
+<template>
+    <table class="table  table-hover general-table">
+        <thead>
+        <tr>
+            <th> Company</th>
+            <th class="hidden-phone">Descrition</th>
+            <th>Profit</th>
+            <th>Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td><a href="#">Graphics</a></td>
+            <td class="hidden-phone">Lorem Ipsum dorolo imit</td>
+            <td>1320.00$ </td>
+            <td><span class="label label-info label-mini">Due</span></td>
+        </tr>
+        <tr>
+            <td>
+                <a href="#">
+                    ThemeBucket
+                </a>
+            </td>
+            <td class="hidden-phone">Lorem Ipsum dorolo</td>
+            <td>556.00$ </td>
+            <td><span class="label label-warning label-mini">Due</span></td>
+        </tr>
+        <tr>
+            <td>
+                <a href="#">
+                    XYZ
+                </a>
+            </td>
+            <td class="hidden-phone">Lorem Ipsum dorolo</td>
+            <td>13240.00$ </td>
+            <td><span class="label label-success label-mini">Paid</span></td>
+        </tr>
+        <tr>
+            <td>
+                <a href="#">
+                    BCSE
+                </a>
+            </td>
+            <td class="hidden-phone">Lorem Ipsum dorolo</td>
+            <td>3455.50$ </td>
+            <td><span class="label label-danger label-mini">Paid</span></td>
+        </tr>
+        <tr>
+            <td><a href="#">AVC Ltd</a></td>
+            <td class="hidden-phone">Lorem Ipsum dorolo imit</td>
+            <td>110.00$ </td>
+            <td><span class="label label-primary label-mini">Due</span></td>
+        </tr>
+        <tr>
+            <td>
+                <a href="#">
+                    Themeforest
+                </a>
+            </td>
+            <td class="hidden-phone">Lorem Ipsum dorolo</td>
+            <td>456.00$ </td>
+            <td><span class="label label-warning label-mini">Due</span></td>
+        </tr>
+
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "Contracts"
+    }
+</script>
+
+<style scoped>
+
+</style>
